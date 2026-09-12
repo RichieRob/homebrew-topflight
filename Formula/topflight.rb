1baf0749258f0070f16b4e9837684c93e033c36d
RichieRob <110105361+RichieRob@users.noreply.github.com>
2026-09-12 18:34:54 +0100

    Update TopFlight to 0.3.1 dependency-free runtime

--- Formula/topflight.rb
+++ Formula/topflight.rb
@@ -1,8 +1,8 @@
 class Topflight < Formula
   desc "Build and run a TopFlight trading bot"
   homepage "https://github.com/RichieRob/topflight-bot"
-  url "https://raw.githubusercontent.com/RichieRob/topflight-bot/v0.3.0/topflight-bot-0.3.0.tgz"
-  sha256 "be34ca700e67e0f4584b22fee0ec2023fd17665f09f363d11e19514b1ee7149d"
+  url "https://raw.githubusercontent.com/RichieRob/topflight-bot/v0.3.1/topflight-bot-0.3.1.tgz"
+  sha256 "76d2fd564379bb2a7195271e23d670cfb041316b65ed7887e137614853871ec9"
   license "MIT"
 
   depends_on "node"
